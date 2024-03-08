27761644ac1cf1ad85e7e38e4e1e3a00b2d0c4d8
ihebbns <123276477+ihebbns@users.noreply.github.com>
2024-03-08 18:11:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,17 +51,17 @@ function App() {
                         </div>
                         <div className="col-md-5 col-lg-4 mx-auto mt-3"> {/* Reduced column size for mobile */}
                           <h6 className="text-uppercase mb-3 font-weight-bold">الاتصال</h6>
-                          <p><i className="fas fa-phone mr-2"></i> <a href="tel:+21646387383" style={{ color: 'blue' }}>+216 46 387 383</a></p>
+                          <p><i className="fas fa-phone mr-2"></i> <a href="tel:+21646387383" style={{ color: 'blue', direction: 'ltr' }}>+216 46 387 383</a></p>
                           <p><i className="fas fa-home mr-2"></i> شارع حمادي الغربي، قليبية 8090، تونس</p>
                           <p><i className="fas fa-envelope mr-2"></i>studiohoussem10@gmail.com</p>
                         </div>
                         <div className="col-md-6 col-lg-2 mx-auto mt-3"> {/* Reduced column size for mobile */}
-                          <h6 className="text-uppercase mb-3 font-weight-bold">تابعنا</h6>
-                          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#3b5998' }} href="https://www.facebook.com/fotografi.studio.houssem"><i className="fab fa-facebook-f fa-square"></i></a>
-                          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#ac2bac' }} href="https://www.instagram.com/studio_houssem_22097666/"><i className="fab fa-instagram fa-square"></i></a>
-                          <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#25D366' }} href="https://api.whatsapp.com/send?phone=+21646387383"><i className="fab fa-whatsapp fa-square"></i></a>
-
+                       <h6 className="text-uppercase mb-3 font-weight-bold">تابعنا</h6>
+                        <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#3b5998' }} href="https://www.facebook.com/fotografi.studio.houssem"><i className="fab fa-facebook-f fa-lg"></i></a>
+                        <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#ac2bac' }} href="https://www.instagram.com/studio_houssem_22097666/"><i className="fab fa-instagram fa-lg"></i></a>
+                       <a className="btn btn-primary btn-floating m-1" style={{ backgroundColor: '#25D366' }} href="https://api.whatsapp.com/send?phone=+21646387383"><i className="fab fa-whatsapp fa-lg"></i></a>
                         </div>
+
                       </div>
                     </section>
                   </div>
